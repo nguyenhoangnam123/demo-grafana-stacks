02010f1cd01eb15194a159cc2f3fdde229a2332f
Nam <namnh21894@gmail.com>
2023-07-20 23:12:32 +0700

    init

---
+++ src/main/java/com/dealmates/demografanastacks/DemoGrafanaStacksApplication.java
@@ -0,0 +1,13 @@
+package com.dealmates.demografanastacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrafanaStacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGrafanaStacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealmates/demografanastacks/DemoGrafanaStacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealmates.demografanastacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrafanaStacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
